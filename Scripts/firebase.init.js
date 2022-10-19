b11723cba644287c7e1e7be4deaeed33cacab875
kishanmishra11 <kishanm.multiqos@gmail.com>
2022-10-19 18:40:02 +0530

    upload all files

---
+++ Scripts/firebase.init.js
@@ -0,0 +1,12 @@
+﻿// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDOlZKt0U91wT3xNWWL0P1tEn-n2uIJKOQ",
+    authDomain: "chat-app-56354.firebaseapp.com",
+    projectId: "chat-app-56354",
+    storageBucket: "chat-app-56354.appspot.com",
+    messagingSenderId: "957505884194",
+    appId: "1:957505884194:web:ee4be6e6568ac3c02dfa7b",
+    measurementId: "G-LLQ4FY40ZM"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
---
+++ messaging/firebase-messaging-sw.js
@@ -0,0 +1,39 @@
+﻿// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here, other Firebase libraries
+// are not available in the service worker.
+importScripts('https://www.gstatic.com/firebasejs/7.6.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/7.6.1/firebase-messaging.js');
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDOlZKt0U91wT3xNWWL0P1tEn-n2uIJKOQ",
+    authDomain: "chat-app-56354.firebaseapp.com",
+    projectId: "chat-app-56354",
+    storageBucket: "chat-app-56354.appspot.com",
+    messagingSenderId: "957505884194",
+    appId: "1:957505884194:web:ee4be6e6568ac3c02dfa7b",
+    measurementId: "G-LLQ4FY40ZM"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
+
+messaging.setBackgroundMessageHandler(function (payload) {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    // Customize notification here
+    const notificationTitle = 'You have new message';
+    const notificationOptions = {
+        body: payload.data.message,
+        icon: payload.data.icon
+    };
+
+    return self.registration.showNotification(notificationTitle,
+        notificationOptions);
+});
+
+
+
